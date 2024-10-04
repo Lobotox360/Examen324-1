5dd2ae8adfe6fe5faa8fb7c04160651a95b508c6
Lobotox360 <lobofabianultimate@gmail.com>
2024-10-04 19:05:01 -0400

    Delete punto6 directory

--- punto6/ABC_catastro/conexion.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","u324","123456","bdfabian");
-?>
--- punto6/ABC_catastro/persona_tiene_propiedades/insertar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include "conexion.inc.php";
-$ci = $_POST['ci'];
-$apellido = $_POST['apellido'];
-$nombre = $_POST['nombre'];
-$sql = "INSERT INTO persona (ci, apellido, nombre, rol, usuario, contrasenia) VALUES ('$ci', '$apellido', '$nombre', 'U', 'NULL', 'NULL')";
-mysqli_query($con, $sql);
-header("Location: ../../index.php")
-?>
